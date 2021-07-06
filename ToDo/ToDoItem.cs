bc4b8fc0667e383f00e16a48f6fe57be8e2f6813
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-06 15:52:05 +0100

    Add constructor and a method to create List of ToDoItems

--- ToDo/ToDoItem.cs
+++ ToDo/ToDoItem.cs
@@ -6,7 +6,24 @@ namespace ToDo
 {
     public class ToDoItem
     {
+        public ToDoItem(string name)
+        {
+            Name = name;
+        }
+
         public string Name { get; set; }
         public bool Completed { get; set; }
+
+        public static IEnumerable<ToDoItem> GetToDoItems()
+        {
+            return new List<ToDoItem>
+            {
+                new ToDoItem("Go to work"),
+                new ToDoItem("Stand-up meeting, with my stand-up colleagues"),
+                new ToDoItem("Lunch time"),
+                new ToDoItem("Go to gym"),
+                new ToDoItem("Family time")
+            };
+        }
     }
 }
