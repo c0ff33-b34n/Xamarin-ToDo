c6dcd16b39819724eb03bc6d55fb2c33808bc68a
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-07 11:58:42 +0100

    Add BoolToTextDecorationConverter and make completed item text strikethrough

---
+++ ToDo/BoolToTextDecorationConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    class BoolToTextDecorationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return TextDecorations.Strikethrough;
+            }
+            return TextDecorations.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
