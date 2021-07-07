76dd6911a9cac8ec6a2522cd3bb2c44f0cd5a680
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-07 13:30:42 +0100

    Add progress bar and count in header of ListView

--- ToDo/ToDoViewModel.cs
+++ ToDo/ToDoViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -10,10 +11,35 @@ namespace ToDo
     class ToDoViewModel : BindableObject
     {
         private ToDoItem _selectedItem;
+        private string _completedHeader;
+        private double _completedProgress;
+
         public ToDoViewModel()
         {
             Items = new ObservableCollection<ToDoItem>(ToDoItem.GetToDoItems());
+            CalculateCompletedHeader();
         }
+        public string CompletedHeader
+        {
+            get => _completedHeader;
+            set
+            {
+                _completedHeader = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double CompletedProgress
+        {
+            get => _completedProgress;
+            set
+            {
+                _completedProgress = value;
+                OnPropertyChanged();
+            }
+        }
+
+
 
         public ObservableCollection<ToDoItem> Items { get; set; }
 
@@ -35,11 +61,19 @@ namespace ToDo
             obj.Completed = true;
             Items.Remove(obj);
             Items.Add(obj);
+            CalculateCompletedHeader();
+        }
+
+        private void CalculateCompletedHeader()
+        {
+            CompletedHeader = $"Completed {Items.Count(x => x.Completed)}/{Items.Count}";
+            CompletedProgress = (double)Items.Count(x => x.Completed) / (double)Items.Count;
         }
 
         private void AddNewItem()
         {
             Items.Add(new ToDoItem($"ToDo Item {Items.Count + 1}"));
+            CalculateCompletedHeader();
         }
     }
 }
